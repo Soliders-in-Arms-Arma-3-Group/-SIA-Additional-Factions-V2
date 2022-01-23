579dba2e3dc1019563cd0068364ee2e3db033ab8
Liam McKendrick <liammckendirckgaming@gmail.com>
2022-01-22 14:37:58 -1000

    Cleaned unecessary entries

--- mod.cpp
+++ mod.cpp
@@ -1,29 +1,8 @@
-// Name of your mod
 name = "SIA Additional Factions v2 (DEV)";
-author = "McKendrick";
-// Picture displayed from the Expansions menu
+author = "Solders in Arms";
 picture = "picture.paa";
-// Display next to the item added by the mod
 logoSmall = "LogoSmall.paa";
-// Logo displayed in the main menu
 logo = "Logo.paa";
-// When the mouse is over, in the main menu
 logoOver = "LogoOver.paa";
-// Website URL, that can accessed from the extansion menu
-//action = "https://community.bistudio.com/wiki/Mod_Presentation";
-// Tool tip displayed when the mouse is left over, in the main menu
-//tooltipOwned = "It's yours!";
-// Color of the extansion (on mouse over)
-dlcColor[] = 
-{	
-	0.23,
-	0.39,
-	0.30,
-	1
-};
-// Overview text, displayed from the extansion menu
+action = "https://github.com/Soliders-in-Arms-Arma-3-Group/SIA-Additional-Factions-V2.git";
 overview = "Adds additional factions.";
-// Hide the extansion name 
-hideName = 0;
-// Hide the extansion menu
-hidePicture = 0;
