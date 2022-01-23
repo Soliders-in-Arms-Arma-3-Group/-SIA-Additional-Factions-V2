1ea39bd3c9d6571467338ad44231b90800bbe9eb
Liam McKendrick <liammckendirckgaming@gmail.com>
2022-01-22 14:38:29 -1000

    Temp revert

--- addons/sia_cia/cfgFactionClasses.hpp
+++ addons/sia_cia/cfgFactionClasses.hpp
@@ -2,8 +2,11 @@ class CfgFactionClasses {
     class sia_faction_cia {
         displayName = "CIA";
         side = 1;
-        flag = "\SIA-Additional-Factions-V2\addons\sia_cia\cia_flag_co.paa";
-        icon = "\SIA-Additional-Factions-V2\addons\sia_cia\cia_icon_co.paa";
+        /*
+        flag = "\sia_cia\addons\sia_cia\cia_flag_co.paa";
+        icon = "\sia_cia\addons\sia_cia\cia_icon_co.paa";
+        */
+        icon = "\rhsusf\addons\rhsusf_main\data\armylogo.paa"; // temp until logi is fixed
         priority = 0;
     };
 };
