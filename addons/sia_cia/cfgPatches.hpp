0f39c005c964a8e5cd9151c1ed7554c59c9da2e3
Liam McKendrick <liammckendirckgaming@gmail.com>
2022-02-09 11:32:29 -1000

    Added units to cfgPatches

--- addons/sia_cia/cfgPatches.hpp
+++ addons/sia_cia/cfgPatches.hpp
@@ -5,7 +5,15 @@ class CfgPatches
 		units[]=
 		{
 			"sia_cia_base",
-            "sia_cia_rifleman"
+            "sia_cia_rifleman",
+			"sia_cia_grenadier",
+			"sia_cia_rifleman_lat",
+			"sia_cia_autorifleman",
+			"sia_cia_marksman",
+			"sia_cia_squadleader",
+			"sia_cia_teamleader",
+			"sia_cia_medic",
+			"sia_cia_engineer"
 		};
 		weapons[] = {};
 		requiredVersion=1.62;
